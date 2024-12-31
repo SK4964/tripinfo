2b11c0625ce43fe89fa56d2d7d02f9796a5dd194
Satheesh Kuselan <satheesh.kuselan@outlook.com>
2024-12-31 13:57:03 +0530

    Build 2024.1231.1356 Promotion changes and data update

---
+++ chunk-26DR4WFC.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-E7LL2NHQ.js";import"./chunk-HKDZNTJD.js";import"./chunk-VH6JMEIQ.js";import"./chunk-FTEUR76U.js";export{a as PromotionsComponent};
--- chunk-PR7G62XM.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-AVBDFR2G.js";import"./chunk-HKDZNTJD.js";import"./chunk-VH6JMEIQ.js";import"./chunk-FTEUR76U.js";export{a as PromotionsComponent};
